5f7a68ec0efd75537003ec7f2f41215321222ebf
Alex Chythlook <achythlook@gmail.com>
2018-06-10 09:40:53 -0800

    Finish setting up and testing ModularEntity

--- src/com/blakwurm/ModularEntity.hx
+++ src/com/blakwurm/ModularEntity.hx
@@ -1,24 +1,38 @@
 package com.blakwurm;
 
 import haxe.ds.HashMap;
+import haxe.ds.Option;
 import DataClass;
 
 using hx.strings.Strings;
 using Type;
+using Map;
+using dataclass.JsonConverter;
 
 class ModularEntity implements DataClass {
 
     public var name: String;
     public var id: String;
 
-    private var modules: Map<String, DataClass>;
+    var modules: Map<String, DataClass> = new Map();
 
-    public function get<T: DataClass>(c: Class<T>) : T {
+    public function get<T: DataClass>(c: Class<T>) : Null<T> {
+        var foo = c.getClassName();
         return cast modules[c.getClassName()];
     }
 
     public function set<T: DataClass>(c: Class<T>, thing: T) : ModularEntity {
+        var foo = c.getClassName();
         modules.set(c.getClassName(), thing);
         return this;
+    }
+
+    public function remove<T: DataClass>(c: Class<T>) : ModularEntity {
+        modules.remove(c.getClassName());
+        return this;
+    }
+
+    public static function copy<T: DataClass>(dataClass: T) : T {
+        return Type.getClass(dataClass).fromJson(dataClass.toJson());
     }
 }
--- test/ModularEntityTest.hx
+++ test/ModularEntityTest.hx
@@ -3,8 +3,10 @@ package ;
 import massive.munit.util.Timer;
 import massive.munit.Assert;
 import massive.munit.async.AsyncFactory;
-import com.blakwurm.ModularEntity;
 import DataClass;
+
+using com.blakwurm.ModularEntity;
+using dataclass.JsonConverter;
 
 
 class ModularEntityTest 
@@ -39,14 +41,70 @@ class ModularEntityTest
 	}
 	
 	@Test
-	public function testExample()
+	public function putsAndGets()
 	{
 		var testA = new TestA({name: "Fern"});
 		testEntity.set(TestA, testA);
 		Assert.areEqual(testA, testEntity.get(TestA));
 	}
+
+	@Test
+	public function replaces() {
+		var testB1 = new TestB({name: "dinghow", tess: 12345, krak: true});
+		var testB2 = testB1.copy();
+		testB2.name = "Xing";
+
+		Assert.areNotEqual(testB1, testB2);
+
+		testEntity.set(TestB, testB1);
+
+		Assert.areEqual(testB1, testEntity.get(TestB));
+		
+		testEntity.set(TestB, testB2);
+
+		Assert.areNotEqual(testB1, testEntity.get(TestB));
+	}
+
+	@Test
+	public function isTypeSafe() {
+		var testA1 = new TestA({name: "thingA"});
+		var testB1 = new TestB({name: "dinghow", tess: 12345, krak: true});
+
+		testEntity.set(TestA, testA1).set(TestB, testB1);
+
+		var testA2 : TestA = testEntity.get(TestA);
+		var testB2 : TestB = testEntity.get(TestB);
+
+		Assert.areEqual(testA1, testA2);
+		Assert.areEqual(testB1, testB2);
+	}
+
+	@Test
+	public function handlesNull() {
+		Assert.isNull(testEntity.get(TestNeverUsed));
+	}
+
+	@Test
+	public function removes() {
+		var testA = new TestA({name: "Hello World"});
+		testEntity.set(TestA, testA);
+		Assert.areEqual(testA, testEntity.get(TestA));
+
+		testEntity.remove(TestA);
+		Assert.isNull(testEntity.get(TestA));
+	}
 }
 
 class TestA implements DataClass {
+	public var name: String;
+}
+
+class TestB implements DataClass {
+	public var tess: Int;
+	public var name: String;
+	public var krak: Bool;
+}
+
+class TestNeverUsed implements DataClass {
 	public var name: String;
 }
