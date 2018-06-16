6b6d72098022a4e2bbd7619883b27b564f3b3411
Zaphodious <achythlook@gmail.com>
2018-06-15 23:40:45 -0800

    Make forward progress on UI

--- src/com/blakwurm/djala/Ui.hx
+++ src/com/blakwurm/djala/Ui.hx
@@ -1,14 +1,23 @@
 package com.blakwurm.djala;
 
 import com.blakwurm.djala.System;
+import haxe.ui.HaxeUIApp;
 import haxe.ui.Toolkit;
 import haxe.ui.components.Button;
+import haxe.ui.core.Component;
 import haxe.ui.core.Screen;
+import haxe.ui.macros.ComponentMacros;
 
 class Ui implements SystemModule {
-    public function preInit(system: System): Bool return true;
-    public function init(system: System): Bool {
+    private var app : HaxeUIApp;
+    public function preInit(system: System): Bool {
         Toolkit.init();
+        app = new HaxeUIApp();
+        return true;
+    }
+    public function init(system: System): Bool {
+        var main:Component = ComponentMacros.buildComponent("resources/ui/shell.xml");
+        app.addComponent(main);
 
         return true;
     }
